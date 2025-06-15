1fdd18f0d5718b454d74efc7571dc3fed6f5917a
Aviral <aviralraj20142222@gmail.com>
2025-06-15 21:15:25 +0530

    keyName

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from openai import OpenAI
 
 app = Flask(__name__)
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv("ChatGptApiKey"))
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
